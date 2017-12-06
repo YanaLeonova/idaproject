c60aa8495dbb06099301cec6f8da718fbd045c96
Jana Leonova <divisiya@gmail.com>
2017-12-06 21:03:01 +0300

    скорректирован js и отправка формы. нет стрелочки в селекте. нет адаптивности

--- js/js.js
+++ js/js.js
@@ -1,51 +1,45 @@
 $(document).ready(function () {
 
-        $.validator.addClassRules({
-            cardNumber1: {
+    //создаю правило для ввода имени владельца карты только латинскими буквами
+    $.validator.addMethod("lettersonly", function (value, element) {
+        return this.optional(element) || /[a-z]+/i.test(value);
+    });
+
+//создаю правила для валидации формы, поиск input ведется по атрибуту name
+    $('#credit-card').validate({
+        rules: {
+            cardNumber1: { //для номера карты 1 поле
+                required: true,
+                rangelength: [4, 4],
+                digits: true
+            },
+            cardNumber2: { //для номера карты 2 поле
+                required: true,
+                rangelength: [4, 4],
+                digits: true
+            },
+            cardNumber3: { //для номера карты 3 поле
+                required: true,
+                rangelength: [4, 4],
+                digits: true
+            },
+            cardNumber4: { //для номера карты 4 поле
                 required: true,
                 rangelength: [4, 4],
                 digits: true
             },
 
-            cardNumber2: {
-                required: true,
-                rangelength: [4, 4],
-                digits: true
-            },
-
-            cardNumber3: {
-                required: true,
-                rangelength: [4, 4],
-                digits: true
-            },
-
-            cardNumber4: {
-                required: true,
-                rangelength: [4, 4],
-                digits: true
-            },
-
-            cardHolder: {
+            cardHolder: { //для имени владельца карты
                 required: true,
                 minlength: 4,
                 lettersonly: true
             },
 
-            cardCvv: {
+            cardCvv: { // для кода cvv
                 required: true,
                 rangelength: [3, 3],
                 digits: true
             }
-        });
-
-
-    $.validator.addMethod( "lettersonly", function( value, element ) {
-        return this.optional( element ) || /^[a-z]+$/i.test( value );
+        }
     });
-
-
-        $('form').validate();
-
-
-    }
-);
+});
